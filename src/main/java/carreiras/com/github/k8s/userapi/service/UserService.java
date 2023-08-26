55e5659697c9323c7c5298d6f23ee4ecdb138256
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-08-26 15:33:53 -0300

    Organizando código

--- src/main/java/carreiras/com/github/k8s/userapi/service/UserService.java
+++ src/main/java/carreiras/com/github/k8s/userapi/service/UserService.java
@@ -27,12 +27,14 @@ public class UserService {
     public UserResponse create(UserRequest userRequest) {
         User user = convertUserRequestToUser(userRequest);
         User savedUser = userRepository.save(user);
+
         return convertUserToUserResponse(savedUser);
 
     }
 
     public List<UserResponse> findAll() {
         List<User> users = userRepository.findAll();
+
         return users.stream()
                 .map(Convert::convertUserToUserResponse)
                 .collect(Collectors.toList());
@@ -41,17 +43,20 @@ public class UserService {
     public UserResponse findById(long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND));
+
         return convertUserToUserResponse(user);
     }
 
     public UserResponse findByCpf(String cpf) {
         User user = userRepository.findByCpf(cpf)
                 .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND));
+
         return convertUserToUserResponse(user);
     }
 
     public List<UserResponse> findByNameContainingIgnoreCase(String name) {
         List<User> users = userRepository.findByNameContainingIgnoreCase(name);
+        
         return users.stream()
                 .map(Convert::convertUserToUserResponse)
                 .collect(Collectors.toList());
